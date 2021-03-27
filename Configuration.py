b0b9a1d9937eee1a51231fa0ae4426f71e00e3d7
orenber <orenber@hotmail.com>
2021-03-27 16:23:09 +0300

    remove error warning in code and typo

--- Configuration.py
+++ Configuration.py
@@ -2,16 +2,19 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 
-config['files'] = {'crome_driver_path': "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"}
+config['files'] = {'chrome_driver_path': "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"}
 
-config['sql'] = {'Driver':"{SQL Server}",
-                 'Server':'ORENBER-PC\MSSQLSERVER2008',
-                 'Database':'RealEstate',
-                 'Trusted_Connection':'yes',
-                 'conection_string':'Driver={SQL Server};Server=ORENBER-PC\\MSSQLSERVER2008;Database=RealEstate;Trusted_Connection=yes;'}
+config['sql'] = {'Driver': "{SQL Server}",
+                 'Server': 'ORENBER-PC\MSSQLSERVER2008',
+                 'Database': 'RealEstate',
+                 'Trusted_Connection': 'yes',
+                 'connection_string': 'Driver={SQL Server};Server=ORENBER-PC\\MSSQLSERVER2008;Database=RealEstate;Trusted_Connection=yes;'}
 
 
-config['url']  = {'site': 'https://www.nadlan.gov.il/'}
+config['url'] = {'site': 'https://www.nadlan.gov.il/'}
+
+config['Estate'] = {'cities': "פתח תקווה",
+                    'street': 'רמב"ם 38'}
 
 
 with open('./setup.ini','w') as f:
--- Estate.py
+++ Estate.py
@@ -7,104 +7,106 @@ from configparser import ConfigParser
 parser = ConfigParser()
 parser.read('setup.ini')
 
+# connect to sql
+conn = pyodbc.connect(parser['sql']['connection_string'])
 
-def export_sql(estat_deal: dict) -> None:
-    for n in range( 0, len( estat_deal ) ):
-        values = (estat_deal[n]["Block"],
-                  estat_deal[n]["Selling_Date"],
-                  estat_deal[n]["Address"],
-                  estat_deal[n]["City"],
-                  estat_deal[n]["Type"],
-                  estat_deal[n]["Rooms"],
-                  estat_deal[n]["Floor"],
-                  estat_deal[n]["Surface"],
-                  estat_deal[n]["Price"],
-                  estat_deal[n]["Change"]);
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM Property')
+
+
+def export_sql(estate_deal: list) -> None:
+    for n in range(0, len(estate_deal)):
+        values = (estate_deal[n]["Block"],
+                  estate_deal[n]["Selling_Date"],
+                  estate_deal[n]["Address"],
+                  estate_deal[n]["City"],
+                  estate_deal[n]["Type"],
+                  estate_deal[n]["Rooms"],
+                  estate_deal[n]["Floor"],
+                  estate_deal[n]["Surface"],
+                  estate_deal[n]["Price"],
+                  estate_deal[n]["Change"])
 
         store_procedure = """
-        exec [RealEstate].[dbo].spInsertDeal
+        exec [RealEstate].[dbo].spInsertDeal 
         @Block = ?,
         @Selling_Date = ?, 
-        @Adresss = ?,
+        @Address = ?,
         @City = ?, 
         @Type_estate = ?, 
         @Rooms = ?,
         @Floor_num = ?,
         @Surface = ?,
         @Price = ?,
-        @Price_change =  ? 
+        @Price_Change =  ? 
         """
 
-        cursor.execute( store_procedure, values[:] )
+        cursor.execute(store_procedure, values[:])
         cursor.commit()
 
         pass
 
 
 def deals_property(deals: list) -> list:
-    value_count = int( len( deals ) )
-    col_num = int( 11 )
-    row_num = int( value_count / col_num )
-    estat_deal = [{}] * row_num
-    estete_row = {
+    value_count = int(len(deals))
+    col_num = int(11)
+    row_num = int(value_count / col_num)
+    estate_deal = [{}] * row_num
+    estate_row = {
         "Block": None, "Selling_Date": None,
         "Address": None, "City": None,
         "Type": None, "Rooms": None,
         "Floor": None, "Surface": None,
         "Price": None, "Change": None
     }
-    row = 0;
-    for n in range( 0, value_count, col_num ):
-        estete_row["Selling_Date"] = datetime.strptime( deals[n], '%d.%m.%Y' ).strftime( "%Y-%m-%d" )
-        estete_row["Address"] = deals[n + 1]
-        estete_row["Block"] = deals[n + 2]
-        estete_row["Type"] = deals[n + 3]
-        estete_row["Rooms"] = float( deals[n + 4] )
-        estete_row["Floor"] = deals[n + 5]
-        estete_row["Surface"] = float( deals[n + 6] )
-        estete_row["Price"] = deals[n + 7]
-        estete_row["Change"] = deals[n + 8]
-        estete_row["City"] = deals[n + 9]
+    row = 0
+    for n in range(0, value_count, col_num):
+        estate_row["Selling_Date"] = datetime.strptime(deals[n], '%d.%m.%Y').strftime("%Y-%m-%d")
+        estate_row["Address"] = deals[n + 1]
+        estate_row["Block"] = deals[n + 2]
+        estate_row["Type"] = deals[n + 3]
+        estate_row["Rooms"] = float(deals[n + 4])
+        estate_row["Floor"] = deals[n + 5]
+        estate_row["Surface"] = float(deals[n + 6])
+        estate_row["Price"] = deals[n + 7]
+        estate_row["Change"] = deals[n + 8]
+        estate_row["City"] = deals[n + 9]
 
-        estat_deal[row] = copy.deepcopy( estete_row )
+        estate_deal[row] = copy.deepcopy(estate_row)
         row = row + 1
 
-    return estat_deal
+    return estate_deal
 
-def import_nadlan_deals(cities: list, street: str) -> dict:
+
+def import_real_estate_deals(city: str, street: str) -> list:
     # That is the site
     main_url = parser['url']['site']
-    path = parser['files']['crome_driver_path']
+    path = parser['files']['chrome_driver_path']
 
-    for city in cities:
-        url = main_url + '/?search=' + city + street  # This way we search every city
-        url = url.replace( " ", "%20" )
-        driver = webdriver.Chrome( path )
-        driver.get( url )
-        column_elements = driver.find_elements_by_class_name( 'tableCol' )
+    url = main_url + '/?search=' + city + " " + street  # This way we search every city
+    url = url.replace(" ", "%20")
+    # connect to the web site
 
-        columns = [s.text for s in column_elements]
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+    column_elements = driver.find_elements_by_class_name('tableCol')
 
-        deals = deals_property( columns )
+    columns = [s.text for s in column_elements]
+
+    deals = deals_property(columns)
     return deals
 
-# connect to sql
-conn = pyodbc.connect( parser['sql']['conection_string'] )
 
-cursor = conn.cursor()
-cursor.execute( 'SELECT * FROM Property' )
+def main():
 
-# connect to the web site
+    es = parser['Estate']
 
-cities = ["פתח תקווה"]
-street = ' רמב"ם 38'
+    # import web table
+    deals = import_real_estate_deals(es['cities'], es['street'])
 
-
+    # export table data to sql
+    export_sql(deals)
 
 
-
-# import web table
-
-
-# exsport table data to sql
-exsport_sql( deals )
+if __name__ == '__main__':
+    main()
